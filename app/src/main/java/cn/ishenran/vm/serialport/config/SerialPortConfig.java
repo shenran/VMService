121d97cbe9c908a52cb8471c327f207d11ca2224
shenran@foxmail.com <jjmm2035>
2018-11-28 17:32:57 +0800

    config

---
+++ app/src/main/java/cn/ishenran/vm/serialport/config/SerialPortConfig.java
@@ -0,0 +1,8 @@
+package cn.ishenran.vm.serialport.config;
+
+public class SerialPortConfig {
+    static public String PATH;
+    static public String BAUD_RATE;
+    static public String DRIVER_JAR;
+
+}
