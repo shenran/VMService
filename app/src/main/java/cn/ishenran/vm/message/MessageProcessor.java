7df677fc99c1d8f1aa99f35f1bcd0d6177875acd
shenran@foxmail.com <jjmm2035>
2018-12-04 10:56:13 +0800

    添加CopyRight

--- app/src/main/java/cn/ishenran/vm/message/MessageProcessor.java
+++ app/src/main/java/cn/ishenran/vm/message/MessageProcessor.java
@@ -1,46 +1,62 @@
+/*
+ * Copyright [2018] [by shenran]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @Author:shenran@ishenran.cn
+ * @LastModified:2018-12-04T10:49:49.421+08:00
+ *
+ *
+ */
 
 package cn.ishenran.vm.message;
 
 import android.util.Log;
-
 import com.alibaba.fastjson.JSONObject;
-
 import java.util.ArrayList;
 import java.util.HashMap;
-
 import cn.ishenran.vm.lib.Decoder;
 import cn.ishenran.vm.lib.Encoder;
 
 public class MessageProcessor {
 
-    static MessageProcessor instance=null;
-    public static MessageProcessor getInstance()
-    {
-        if(instance==null)
-            instance =new MessageProcessor();
+    static MessageProcessor instance = null;
+
+    public static MessageProcessor getInstance() {
+        if (instance == null)
+            instance = new MessageProcessor();
         return instance;
     }
 
-    private Encoder m_encoder=null;
-    private Decoder m_decoder=null;
-    public void init(Encoder encoder, Decoder decoder)
-    {
-        m_encoder=encoder;
-        m_decoder=decoder;
-        WriteMessageQueue=m_encoder.getWriteMessageQueue();
-        if(WriteMessageQueue==null)
-            WriteMessageQueue=new ArrayList<byte[]>();
+    private Encoder m_encoder = null;
+    private Decoder m_decoder = null;
+
+    public void init(Encoder encoder, Decoder decoder) {
+        m_encoder = encoder;
+        m_decoder = decoder;
+        WriteMessageQueue = m_encoder.getWriteMessageQueue();
+        if (WriteMessageQueue == null)
+            WriteMessageQueue = new ArrayList<byte[]>();
         StartThread();
     }
 
 
-    public ArrayList<byte[]> ReadMessageQueue=new ArrayList<byte[]>();
-    public ArrayList<byte[]> WriteMessageQueue=null;
-    private boolean isRunning=false;
+    public ArrayList<byte[]> ReadMessageQueue = new ArrayList<byte[]>();
+    public ArrayList<byte[]> WriteMessageQueue = null;
+    private boolean isRunning = false;
 
 
-    public void AddReadMessageQueue(byte[] bytes)
-    {
+    public void AddReadMessageQueue(byte[] bytes) {
         ReadMessageQueue.add(bytes);
         //if(MessageQueue.)
         synchronized (ReadMessageQueue) {
@@ -49,40 +65,35 @@ public class MessageProcessor {
 
     }
 
-    public void StopThread()
-    {
-        isRunning=false;
+    public void StopThread() {
+        isRunning = false;
         synchronized (ReadMessageQueue) {
             ReadMessageQueue.notifyAll();
         }
     }
 
-    public void StartThread()
-    {
+    public void StartThread() {
 
 
-
-        Thread t=new Thread(new Runnable() {
+        Thread t = new Thread(new Runnable() {
             @Override
             public void run() {
-                isRunning=true;
+                isRunning = true;
                 Log.d("MessageProcessor", "run: ");
                 while (isRunning) {
 
                     if (!ReadMessageQueue.isEmpty()) {
-                        JSONObject rev=m_decoder.decode(ReadMessageQueue.get(0));
-                        Integer id=m_decoder.getCallFunById(rev);
-                        RevReturnMessage(id,rev);
+                        JSONObject rev = m_decoder.decode(ReadMessageQueue.get(0));
+                        Integer id = m_decoder.getCallFunById(rev);
+                        RevReturnMessage(id, rev);
                         ReadMessageQueue.remove(0);
-                    }
-                    else
-                    {
+                    } else {
                         try {
                             synchronized (ReadMessageQueue) {
                                 ReadMessageQueue.wait();
                             }
                         } catch (InterruptedException e) {
-                            Log.e(this.getClass().getName(), "run: ",e );
+                            Log.e(this.getClass().getName(), "run: ", e);
                         }
                     }
                 }
@@ -92,19 +103,18 @@ public class MessageProcessor {
 
     }
 
-    public void AddWriteMessage(JSONObject msg,FunctionReturn obj)
-    {
+    public void AddWriteMessage(JSONObject msg, FunctionReturn obj) {
 
-        Integer FunId=m_encoder.encode(msg);
-        CallFunctionQueue.put(FunId,obj);
+        Integer FunId = m_encoder.encode(msg);
+        CallFunctionQueue.put(FunId, obj);
     }
 
-    public void RevReturnMessage(Integer id,JSONObject msg) {
-        FunctionReturn obj =CallFunctionQueue.get(id);
-        if(obj!=null) {
-            obj.rev=msg;
-            obj.status=200;
-            Log.d("RevReturnMessage", "RevReturnMessage: "+obj.id);
+    public void RevReturnMessage(Integer id, JSONObject msg) {
+        FunctionReturn obj = CallFunctionQueue.get(id);
+        if (obj != null) {
+            obj.rev = msg;
+            obj.status = 200;
+            Log.d("RevReturnMessage", "RevReturnMessage: " + obj.id);
             synchronized (obj) {
                 obj.notifyAll();
             }
@@ -112,10 +122,8 @@ public class MessageProcessor {
         }
     }
 
-    public byte[] getWriteBuffer()
-    {
-        if(WriteMessageQueue.isEmpty())
-        {
+    public byte[] getWriteBuffer() {
+        if (WriteMessageQueue.isEmpty()) {
             try {
                 synchronized (WriteMessageQueue) {
                     WriteMessageQueue.wait();
@@ -125,11 +133,11 @@ public class MessageProcessor {
             }
         }
         m_encoder.WaitWrite();
-        byte[] buffer= WriteMessageQueue.get(0);
+        byte[] buffer = WriteMessageQueue.get(0);
         WriteMessageQueue.remove(0);
         return buffer;
     }
 
-    HashMap<Integer,FunctionReturn> CallFunctionQueue=new HashMap<Integer,FunctionReturn>();
+    HashMap<Integer, FunctionReturn> CallFunctionQueue = new HashMap<Integer, FunctionReturn>();
 
 }
--- app/src/main/java/cn/ishenran/vm/serialport/SerialPortProcessor.java
+++ app/src/main/java/cn/ishenran/vm/serialport/SerialPortProcessor.java
@@ -1,21 +1,31 @@
+/*
+ * Copyright [2018] [by shenran]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @Author:shenran@ishenran.cn
+ * @LastModified:2018-12-04T10:49:49.421+08:00
+ *
+ *
+ */
+
 package cn.ishenran.vm.serialport;
 
-import android.content.Context;
-import android.os.Environment;
 import android.serialport.api.SerialPort;
 import android.util.Log;
-
-import com.alibaba.fastjson.JSONObject;
-
-import java.io.File;
 import java.io.IOException;
-
-
 import cn.ishenran.vm.lib.Decoder;
 import cn.ishenran.vm.lib.Encoder;
-import cn.ishenran.vm.message.MessageProcessor;
-import dalvik.system.DexClassLoader;
-
 
 public class SerialPortProcessor {
 
